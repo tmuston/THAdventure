4ae4a5ce09a43f9de2684a969e1a05371cf3d0f9
tim@tmuston.co.uk <tim@tmuston.co.uk>
2020-05-02 00:41:29 +0100

    Add project files.

---
+++ cApp.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "wx/wx.h"
+#include "cMain.h"
+class cApp : public wxApp
+{
+public:
+	cApp();
+	~cApp();
+	virtual bool OnInit();
+private:
+	cMain* m_frame1 = nullptr;
+};
+
---
+++ cMain.cpp
@@ -0,0 +1,10 @@
+#include "cMain.h"
+
+cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Town Hall Text Adventure - episode one:  The hunt for Henry", wxDefaultPosition, wxSize(800, 600), wxDEFAULT_FRAME_STYLE & ~wxRESIZE_BORDER & ~wxMAXIMIZE_BOX)
+{
+	Centre();
+}
+
+cMain::~cMain()
+{
+}
